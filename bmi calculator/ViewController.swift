bd948d40a88068a8d5ff434a6bba4505431b0ecb
FaiazIbraev <fayaz.ibraev@gmail.com>
2022-06-30 11:07:11 +0600

    Initial Commit

---
+++ bmi calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmi calculator
+//
+//  Created by Faiaz Ibraev on 30/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
