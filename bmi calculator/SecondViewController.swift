f6d8fe290ff2083c46ff597b22341f376381f0ca
FaiazIbraev <fayaz.ibraev@gmail.com>
2022-06-30 20:35:37 +0600

    new edition

--- bmi calculator/SecondViewController.swift
+++ bmi calculator/SecondViewController.swift
@@ -8,22 +8,70 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var backToMainButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var adviceTitle: UILabel!
+    @IBOutlet weak var adviceMain: UILabel!
+    @IBOutlet weak var heightLabelSecond: UILabel!
+    @IBOutlet weak var weightLabelSecond: UILabel!
+    @IBOutlet weak var smileImage: UIImageView!
+    @IBOutlet weak var recipeButton: UIButton!
+    
+    
+    var resultSecond: Double = 0
+    var secondHeight = ""
+    var secondWeight = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        getResult()
+        getData()
+        recipe()
+        recipeButton.isHidden = true
+        
+//        self.navigationItem.setHidesBackButton(true, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButton(_ sender: UIButton) {
+        self.dismiss(animated: true)
     }
-    */
+    
+    func recipe(){
+        recipeButton.layer.borderWidth = 1
+        recipeButton.layer.cornerRadius = 15
+        recipeButton.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func getData(){
+        heightLabelSecond.text = secondHeight
+        weightLabelSecond.text = secondWeight
+    }
+
+    func getResult(){
+        if resultSecond < 18.5 {
+            adviceTitle.text = "У вас недовес"
+            adviceMain.text = "Обратитесь к врачу"
+            recipeButton.isHidden = false
+        } else if resultSecond > 18.5 && resultSecond < 25 {
+            adviceTitle.text = "У вас вес в норме"
+            adviceMain.text = "Вы молодец!"
+        } else if resultSecond > 25 && resultSecond < 30 {
+            adviceTitle.text = "У вас вес выше нормы"
+            adviceMain.text = "Нужно занятьс спортом"
+        }else if resultSecond > 30{
+            adviceTitle.text = "У вас ожирение"
+            adviceMain.text = "Нужно срочно сесть на диету"
+        }
+    }
+    
+    
+    
+    @IBAction func backToMainButtonTapped(_ sender: UIButton) {
+    }
+    
+    
 
 }
--- bmi calculator/ViewController.swift
+++ bmi calculator/ViewController.swift
@@ -9,10 +9,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var getResultButton: UIButton!
+    
+    var weightSliderPosition = 50
+    var heightSliderPosition = 150
+    
+    var result: Double = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  
+        setup ()
     }
+
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        restart()
+//    }
+    
+    func restart (){
+        weightLabel.text = "00"
+        heightLabel.text = "00"
+    }
+    
+    func setup (){
+        weightLabel.layer.masksToBounds = true
+        weightLabel.layer.cornerRadius = 15
+        heightLabel.layer.masksToBounds = true
+        heightLabel.layer.cornerRadius = 15
+        getResultButton.layer.masksToBounds = true
+        getResultButton.layer.cornerRadius = 15
+        heightLabel.text = String ("\(heightSliderPosition) см")
+        weightLabel.text = String ("\(weightSliderPosition) кг")
+        
+    }
+    
+    
+    @IBAction func weigthSliderChange(_ sender: UISlider) {
+        weightSliderPosition = Int(weightSlider.value)
+        weightLabel.text = String ("\(weightSliderPosition) кг")
+        
+    }
+    
+    
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        heightSliderPosition = Int(heightSlider.value)
+        heightLabel.text = String ("\(heightSliderPosition) см")
+    }
+    
+    
+    @IBAction func getResultButtonTapped(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        navigationController?.pushViewController(vc, animated: true)
+        vc.resultSecond = self.result
+        vc.secondWeight = "\(weightSliderPosition) кг"
+        vc.secondHeight = "\(heightSliderPosition) см"
+        result = Double(weightSliderPosition)/Double(heightSliderPosition*heightSliderPosition)
+        print(result)
+    }
+    
 
 
 }
